8388eaa602493b2d7491dca4becfcc46c09f0550
donup3 <donup08@naver.com>
2021-09-09 18:38:17 +0900

    이코테 Part3 정렬 update

---
+++ 이코테/Part3/정렬/카드정렬하기.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+sys.stdin = open("input.txt", "r")
+
+n=int(input())
+heap=[]
+for i in range(n):
+  data=int(input())
+  heapq.heappush(heap,data)
+
+result=0
+while len(heap)!=1:
+  a=heapq.heappop(heap)
+  b=heapq.heappop(heap)
+  sum_value=a+b
+  result+=sum_value
+  heapq.heappush(heap,sum_value)
+
+print(result)
