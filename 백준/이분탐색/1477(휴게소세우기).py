174ec977361b07fa2ce9442e725c365b4c244e66
donup3 <donup08@naver.com>
2022-04-27 18:42:34 +0900

    백준 이분탐색 update

---
+++ 백준/이분탐색/1477(휴게소세우기).py
@@ -0,0 +1,25 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+n, m, l = map(int, input().split())
+rests = sorted([0] + list(map(int, input().split())) + [l])
+
+low = 1
+high = l
+answer = high
+while low <= high:
+    mid = (low + high) // 2
+    
+    cnt = 0
+    for i in range(n+1):
+        cnt += (rests[i+1] - rests[i] - 1) // mid 
+        
+    if cnt<=m:
+        answer = mid
+        high = mid - 1
+    else:
+        low = mid + 1
+        
+print(answer)
